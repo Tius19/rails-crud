d0eb7578e9b93896bcf6b6226e5e45632631dbe8
Leo DHotman <dhotman19@yahoo.com>
2023-10-15 12:26:34 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new", as: "new_restaurant"
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id/edit", to: "restaurants#edit", as: "edit_restaurant"
+  patch "restaurants/:id", to: "restaurants#update"
+
+  get "restaurants/:id", to: "restaurants#show"
 end
